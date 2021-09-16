5495fc2f306b11512d0e366c42d5f35b9ae203f4
praveshtiwari701 <90543828+praveshtiwari701@users.noreply.github.com>
2021-09-16 08:50:06 +0530

    Add files via upload

---
+++ oddoreven.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("Enter a Number\n");
+    scanf("%d",&i);
+    if(i%2==0)
+    {
+        printf("The number %d is Even",i);
+    }
+    else
+    {
+        printf("The number %d is Odd",i);
+    }
+    printf("\n");
+}
+    
